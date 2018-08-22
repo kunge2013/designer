7c30828dd713f2c0cab610a1518a7391f62dbe3e
yimo <76826040@qq.com>
2018-08-22 15:21:20 +0800

    no commit message

--- src/main/java/com/yimo/btem/designpattern/decorator/Decorator.java
+++ src/main/java/com/yimo/btem/designpattern/decorator/Decorator.java
@@ -20,43 +20,44 @@ public abstract class Decorator extends Component {
 
     @Override
     public void operation() {
-	if (component != null) {
-	    component.operation();
-	}
+        if (component != null) {
+            component.operation();
+        }
     }
 
 }
 
 class ConcreteDecoratorA extends Decorator {
+    // 本类的独有功能，以区别于ConcreteDecoratorB
     private String addedState;
 
     @Override
     public void operation() {
-	// 首先运行原Component的operation()，再执行本类的功能，如addedState，相当于对原Component进行了装饰
-	super.operation();
-	addedState = "A中的new state ";
-	System.out.println(addedState + "具体装饰对象A的操作");
+        // 首先运行原Component的operation()，再执行本类的功能，如addedState，相当于对原Component进行了装饰
+        super.operation();
+        addedState = "A中的new state ";
+        System.out.println(addedState + "具体装饰对象A的操作");
     }
 }
 
 class ConcreteDecoratorB extends Decorator {
     @Override
     public void operation() {
-	super.operation();
-	addedBehavior();
-	System.out.println("具体装饰对象B的操作");
+        super.operation();
+        addedBehavior();
+        System.out.println("具体装饰对象B的操作");
     }
 
     public void addedBehavior() {
-	System.out.print("B中的新增行为 ");
+	    System.out.print("B中的新增行为 ");
     }
 }
 
 class ConcreteDecoratorC extends Decorator {
     @Override
     public void operation() {
-	super.operation();
-	System.out.println("C没有特殊行为 " + "具体装饰对象C的操作");
+        super.operation();
+        System.out.println("C没有特殊行为 " + "具体装饰对象C的操作");
     }
 
 }
